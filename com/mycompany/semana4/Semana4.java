eca961bf6561df2e1f135f6c4ffa8438b826e85d
ccapacca <142861129+ccapacca@users.noreply.github.com>
2024-04-21 09:49:34 -0500

    Add files via upload

---
+++ com/mycompany/semana4/Semana4.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.semana4;
+
+import pantallas.pantallaCalculadora;
+
+public class Semana4 {
+
+    public static void main(String[] args) 
+    {
+        pantallaCalculadora pantalla =new pantallaCalculadora();
+        pantalla.setVisible(true);
+        
+    }
+}
